137df847da03663cda36325d7ce8fe1138d6b73b
Sune <enxbox@hotmail.com>
2019-12-09 13:35:49 +0100

    Add project configs

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Nikolaj Schlüter and Sune Klem on 09/12/2019.
+//
+#include <iostream>
+
+int main(){
+    std::cout << "https://github.com/mortennobel/SimpleRenderEngineProject" << std::endl;
+
+    std::cout << "I Chose you!" << std::endl;
+
+    return 0;
+}
